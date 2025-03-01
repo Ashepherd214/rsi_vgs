dda4402ce7d3ffb2341720752070cb97e8f89b33
ashepherd214 <ashepherd214@gmail.com>
2025-03-01 04:13:17 -0600

    Changed Button locations and layout

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Visual Ground Segment',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 45, 103, 201)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 45, 103, 201)),
         useMaterial3: true,
       ),
       home: Scaffold(
@@ -44,7 +45,8 @@ class SelectionTable extends StatefulWidget {
 }
 
 class _SelectionTableState extends State<SelectionTable> {
-  Future<List<Map<String, dynamic>>> readData() async {
+  // --- Data Fetching ---
+  Future<List<Map<String, dynamic>>> readAircraftData() async {
     final db = FirebaseFirestore.instance;
     final querySnapshot = await db.collection("Aircrafts").get();
     return querySnapshot.docs
@@ -55,35 +57,49 @@ class _SelectionTableState extends State<SelectionTable> {
         .toList();
   }
 
-  // State variables
-  int? _selectedAircraftIndex; // to keep track of selection
+  Future<List<Map<String, dynamic>>> readRunwayData() async {
+    final db = FirebaseFirestore.instance;
+    final querySnapshot = await db.collection("Runways").get();
+    return querySnapshot.docs
+        .map((doc) => {
+              'id': doc.id,
+              ...doc.data(),
+            })
+        .toList();
+  }
+
+  // --- State Variables ---
+  int? _selectedAircraftIndex;
+  int? _selectedRunwayIndex;
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
         Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            FutureBuilder<List<Map<String, dynamic>>>(
-              future: readData(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Text('No data available');
-                } else {
-                  final data = snapshot.data!;
-                  return Expanded(
-                    child: SizedBox(
+            // --- Aircraft Column ---
+            Expanded(
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: readAircraftData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Text('No aircraft data available');
+                  } else {
+                    final aircraftData = snapshot.data!;
+                    return SizedBox(
                       height: 400,
                       child: ListView.builder(
-                        itemCount: data.length,
+                        itemCount: aircraftData.length,
                         itemBuilder: (context, index) {
                           final isSelected = _selectedAircraftIndex == index;
                           return ListTile(
-                            title: Text(data[index]['id']),
+                            title: Text(aircraftData[index]['id']),
                             selected: isSelected,
                             selectedTileColor: Colors.blue[100],
                             onTap: () {
@@ -91,64 +107,97 @@ class _SelectionTableState extends State<SelectionTable> {
                                 _selectedAircraftIndex =
                                     isSelected ? null : index;
                               });
-                              print('Selected Aircraft ID: ${data[index]['id']}');
-                              // Here you can implement what you want to do with the ID.
+                              print('Selected Aircraft ID: ${aircraftData[index]['id']}');
                             },
                           );
                         },
                       ),
-                    ),
-                  );
-                }
-              },
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: 400,
-                color: Colors.grey[300],
-                child: Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
-                  const SizedBox(height: 15),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue, foregroundColor: Colors.white),
-                    child: const Text("Add Aircraft"),
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          elevation: 100),
-                      child: const Text("Add Runway")),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          elevation: 100),
-                      child: const Text("Delete Aircraft")),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          elevation: 100),
-                      child: const Text("Delete Runway")),
-                  const SizedBox(height: 90),
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          elevation: 100),
-                      child: const Text("Calculate VGS")),
-                ]),
+                    );
+                  }
+                },
               ),
+            ),
+            // --- Runway Column ---
+            Expanded(
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: readRunwayData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Text('No runway data available');
+                  } else {
+                    final runwayData = snapshot.data!;
+                    return SizedBox(
+                      height: 400,
+                      child: ListView.builder(
+                        itemCount: runwayData.length,
+                        itemBuilder: (context, index) {
+                          final isSelected = _selectedRunwayIndex == index;
+                          return ListTile(
+                            title: Text(runwayData[index]['id']),
+                            selected: isSelected,
+                            selectedTileColor: Colors.blue[100],
+                            onTap: () {
+                              setState(() {
+                                _selectedRunwayIndex =
+                                    isSelected ? null : index;
+                              });
+                              print('Selected Runway ID: ${runwayData[index]['id']}');
+                            },
+                          );
+                        },
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+// Row(
+//   children: [
+//     Expanded(
+//       flex: 1,
+//       child: Container(
+//         color: Colors.grey[300],
+//         padding: const EdgeInsets.all(16.0), // Added padding for spacing
+//         child: 
+        Row( // Changed Column to Row
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Added for spacing
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white),
+              child: const Text("Add Aircraft"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  elevation: 100),
+              child: const Text("Delete Aircraft"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  elevation: 100),
+              child: const Text("Add Runway"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  elevation: 100),
+              child: const Text("Delete Runway"),
             ),
           ],
         ),
@@ -162,27 +211,38 @@ class _SelectionTableState extends State<SelectionTable> {
                   flex: 3,
                   child: Container(
                     height: 50,
-                    color: Colors.purple,
-                    // New: Use a child widget to display the selected ID
-                    child: Center(child: SelectedAircraftIdWidget(selectedAircraftIndex: _selectedAircraftIndex)),
+                    //color: Colors.purple,
+                    child: Center(
+                        child: SelectedAircraftIdWidget(
+                            selectedAircraftIndex: _selectedAircraftIndex)),
                   ),
                 ),
                 Expanded(
                   flex: 3,
                   child: Container(
                     height: 50,
-                    color: Colors.yellow,
+                    //color: Colors.yellow,
+                    child: Center(
+                        child: SelectedRunwayIdWidget(
+                            selectedRunwayIndex: _selectedRunwayIndex)),
                   ),
                 ),
                 Expanded(
                   flex: 1,
                   child: Container(
                     height: 50,
-                    color: Colors.orange,
-                  ),
-                ),
-              ],
-            ),
+                    //color: Colors.orange,
+                        child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  foregroundColor: Colors.white,
+                                  elevation: 100),
+                                  child: const Text("Calculate VGS")),
+      ),
+    ),
+  ],
+),
           ],
         ),
         Row(
@@ -211,7 +271,8 @@ class _SelectionTableState extends State<SelectionTable> {
 class SelectedAircraftIdWidget extends StatelessWidget {
   final int? selectedAircraftIndex;
 
-  const SelectedAircraftIdWidget({Key? key, this.selectedAircraftIndex}) : super(key: key);
+  const SelectedAircraftIdWidget({Key? key, this.selectedAircraftIndex})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -219,11 +280,36 @@ class SelectedAircraftIdWidget extends StatelessWidget {
       return const Text("No aircraft selected");
     }
     return FutureBuilder<List<Map<String, dynamic>>>(
-      future: _SelectionTableState().readData(),
+      future: _SelectionTableState().readAircraftData(),
       builder: (context, snapshot) {
-        if(snapshot.hasData){
+        if (snapshot.hasData) {
           final data = snapshot.data!;
           return Text(data[selectedAircraftIndex!]['id'].toString());
+        } else {
+          return const Text("Loading");
+        }
+      },
+    );
+  }
+}
+
+class SelectedRunwayIdWidget extends StatelessWidget {
+  final int? selectedRunwayIndex;
+
+  const SelectedRunwayIdWidget({Key? key, this.selectedRunwayIndex})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (selectedRunwayIndex == null) {
+      return const Text("No runway selected");
+    }
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: _SelectionTableState().readRunwayData(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final data = snapshot.data!;
+          return Text(data[selectedRunwayIndex!]['id'].toString());
         } else {
           return const Text("Loading");
         }
